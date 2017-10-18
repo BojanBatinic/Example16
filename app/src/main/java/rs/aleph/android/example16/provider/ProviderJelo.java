319378873cb00ef8a23f04a6aa8b7443aef7b795
BojanBatinic <bojanbatinic77@gmail.com>
2017-10-18 20:37:45 +0200

    Zadatak sa termina 16 - ispravka

--- app/src/main/java/rs/aleph/android/example16/provider/ProviderJelo.java
+++ app/src/main/java/rs/aleph/android/example16/provider/ProviderJelo.java
@@ -20,7 +20,7 @@ public class ProviderJelo {
 
         List<Jelo> jela = new ArrayList<>();
         jela.add(new Jelo(0, "sopska.jpg", "Sopska salata 400g", "Osvezavajuca salata od svezeg povrca i sira", "Paradajiz, krastavac, paprika i tvrdi sir.", 190.00, 300.00, mrsnoJelo ));
-        jela.add(new Jelo(1, "cezar.jpg", "Cezar salata 400g", "Obrok salata od svezeg povrca i mesa", "Paradajiz, krastavac, pilece meso, dresing i hleb u kockicama.", 210.00, 350.00, mrsnoJelo));
+        jela.add(new Jelo(1, "cezar.jpg", "Cezar salata 400g", "Obrok salata od svezeg povrca i mesa", "Paradajiz, kupus, krastavac, pilece meso, dresing i hleb u kockicama.", 210.00, 350.00, mrsnoJelo));
         jela.add(new Jelo(2, "tuna.jpg", "Tuna salata 400g", "Obrok salata od svezeg povrca i tunjevine", "Paradajiz, krastavac, paprika i tunjevina u komadicima.", 230.00, 320.00, posnoJelo));
         jela.add(new Jelo(3, "vegeterijana.jpg", "Vegeterijanska salata 400g", "Osvezavajuca salata od svezeg povrca", "Paradajiz, krastavac, paprika, tikvice i patlidzan.", 170.00, 280.00, posnoJelo));
         return jela;
